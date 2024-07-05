7f536cf2d033344ae1b2de9cd8772f058dd82956
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-07-05 12:45:55 +0000

    install clean blog bootstrap theme and add header image

--- run.py
+++ run.py
@@ -19,6 +19,10 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
