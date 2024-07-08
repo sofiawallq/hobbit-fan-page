a872128b9bc31e4e356322da3e91492e41974c87
Sofia Wallqvist <sofiawallqvist@gmail.com>
2024-07-08 09:31:58 +0000

    add text and images to about page

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask, render_template
 
@@ -12,20 +13,22 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=False)
+        debug=True)
+
